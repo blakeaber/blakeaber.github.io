0c0c57043feca6bcdea30a198eda642b393fbc59
Blake Aber <blake.aber@gmail.com>
2023-01-25 11:29:09 -0500

    first commit

---
+++ src/gatsby-theme-mate/icons.js
@@ -0,0 +1,20 @@
+// Please check the customization documentation in case you want to modify the base project --> https://github.com/EmaSuriano/gatsby-starter-mate#customization
+import {
+    faMedium,
+    faLinkedin,
+    faGithub,
+  } from '@fortawesome/free-brands-svg-icons';
+  import {
+    faGlobe,
+    faEnvelope,
+    faCoffee,
+  } from '@fortawesome/free-solid-svg-icons';
+  
+  export const ICONS = [
+    faMedium,
+    faLinkedin,
+    faGithub,
+    faGlobe,
+    faEnvelope,
+    faCoffee,
+  ];
